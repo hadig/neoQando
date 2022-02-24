10d1b2f78c99812638f251998a62ee8d1afd5b48
hadig <4592275+hadig@users.noreply.github.com>
2022-02-24 19:33:08 +0100

    Change the look of the data displayed

--- neoQando/Views/Here.swift
+++ neoQando/Views/Here.swift
@@ -14,19 +14,8 @@ struct Here: View {
             MapView()
             List {
                 ForEach(vm.monitor) { monitor in
-                    let title = monitor.locationStop.properties.title
-                    let line = monitor.lines[0]
+                    MonitorDataRow(monitor: monitor)
                     
-                    if( title != line.towards) {
-                        Text(title).bold()
-                        VStack(alignment: .leading) {
-                            Text(line.name).bold()
-                            Text("➡️ \(line.towards)")
-                            
-                            line.realtimeSupported ?
-                            Text("🟢") : Text("🔴")
-                        }
-                    }
                 }
             }
             .navigationBarTitle("")
@@ -38,8 +27,64 @@ struct Here: View {
     }
 }
 
+struct MonitorDataRow: View {
+    var monitor: RealTime.Data.Monitor
+    var body: some View {
+        let title = monitor.locationStop.properties.title
+        let line = monitor.lines[0]
+        let lineTime = 4
+        
+        HStack {
+            VStack (alignment: .leading, spacing: 10) {
+                HStack {
+                    ZStack {
+                        RoundedRectangle(cornerRadius: 5)
+                            .fill(Color.black)
+                            .frame(width: 40, height: 30)
+                        Text(line.name)
+                            .foregroundColor(Color.white)
+                            .bold()
+                    }
+                    Text(title)
+                        .bold()
+                }
+                
+                
+                HStack {
+                    Image(systemName: "arrow.right")
+                    Text("\(line.towards)")
+                }
+                
+                if (!line.realtimeSupported) {
+                    HStack {
+                        Image(systemName: "circle.fill")
+                            .foregroundColor(.red)
+                        Text("No Realtime Data")
+                    }
+                    .font(.system(size: 10, weight: .light))
+                }
+                
+                
+            }
+            
+            Spacer()
+            
+            VStack (spacing: 10) {
+                Text("\(lineTime) min.")
+                    .underline()
+                Text("\(lineTime) min.")
+                    .foregroundColor(.secondary)
+            }
+        }
+        .padding(.top)
+        .padding(.bottom)
+        
+    }
+}
+
 struct Here_Previews: PreviewProvider {
     static var previews: some View {
         Here(vm: MonitorViewModel())
+            .previewInterfaceOrientation(.portrait)
     }
 }
